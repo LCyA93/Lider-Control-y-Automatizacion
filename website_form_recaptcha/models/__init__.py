2cbbab26f98e14c506d4157cce1b42176695d1a4
LCyA93 <37308705+LCyA93@users.noreply.github.com>
2024-10-25 17:25:10 -0600

    Delete website_form_recaptcha directory

--- website_form_recaptcha/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import ir_model
-from . import website
-from . import website_config_settings
-from . import website_form_recaptcha
--- website_form_recaptcha/readme/ROADMAP.rst
+++
@@ -1,3 +0,0 @@
-* Add domain validation
-* Split recaptcha APIs from `website_form` dependency
-  (ie: new module `website_recaptcha_base`)
