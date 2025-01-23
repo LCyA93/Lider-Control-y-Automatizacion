15f22f80593ceafb8b7482483f43aaaaa7e20d72
LCyA93 <37308705+LCyA93@users.noreply.github.com>
2025-01-23 15:02:17 -0600

    Update __manifest__.py

--- lidercontrol-16.0/l10n_mx_adenda_ext/__manifest__.py
+++ lidercontrol-16.0/l10n_mx_adenda_ext/__manifest__.py
@@ -7,6 +7,7 @@
 {
     'name': 'Addenda Order ID',
     'version': '17.01',
+    'license': 'LGPL-3',
     'description': ''' Agrega campos para agregar una addenda.
     ''',
     'category': 'Accounting',
